af07c5babe7c1fa29340baaf0a589f4c281b62eb
aazucena <aldrinp.azucena@gmail.com>
2025-11-18 19:39:30 -0700

    Integrated redis to Strapi CMS

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,4 +1,43 @@
 export default ({ env }) => ({
+  redis: {
+    config: {
+      settings: {
+        debug: false,
+        debugIORedis: false,
+        redlockConfig: {
+          driftFactor: 0.01,
+          retryCount: 10,
+          retryDelay: 200,
+          retryJitter: 200,
+        },
+        enableRedlock: false,
+        lockDelay: null,
+        lockTTL: 5000,
+      },
+      connections: {
+        default: {
+          connection: {
+            host: env('REDIS_HOST', '127.0.0.1'),
+            port: env.int('REDIS_PORT', 6379),
+            db: 0,
+          },
+        },
+      },
+    },
+  },
+  "rest-cache": {
+    config: {
+      provider: {
+          name: "redis",
+          options: {
+              // The name of the connection as defined in the Redis plugin.
+              connection: "default",
+              // The time to live in milliseconds. This is the maximum amount of time that an item can be in the cache before it is removed.
+              ttl: 3600 * 1000
+          },
+      },
+    }
+  },
   upload: {
     config: {
       provider: 'cloudinary',
--- apps/cms/plugins.sh
+++ apps/cms/plugins.sh
@@ -7,7 +7,10 @@ pnpm add @strapi/provider-upload-cloudinary \
   @_sh/strapi-plugin-ckeditor \
   strapi-plugin-multi-select \
   strapi-advanced-uuid \
-  @strapi/plugin-color-picker
+  @strapi/plugin-color-picker \
+  @strapi-community/plugin-rest-cache \
+  @strapi-community/plugin-redis \
+  @strapi-community/provider-rest-cache-redis
 
 pnpm i --ignore-workspace
 docker compose build --no-cache
