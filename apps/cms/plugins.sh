16ea98b4470283b5c5ff44d9cfce43178c56a623
aazucena <aldrinp.azucena@gmail.com>
2025-11-18 20:17:29 -0700

    Add Strapi PReview Button PLugin

--- apps/cms/plugins.sh
+++ apps/cms/plugins.sh
@@ -8,9 +8,13 @@ pnpm add @strapi/provider-upload-cloudinary \
   strapi-plugin-multi-select \
   strapi-advanced-uuid \
   @strapi/plugin-color-picker \
+  strapi-plugin-preview-button \
   @strapi-community/plugin-rest-cache \
   @strapi-community/plugin-redis \
   @strapi-community/provider-rest-cache-redis
+  # strapi-plugin-navigation \
+  # @strapi/plugin-i18n \
+  # strapi-plugin-sitemap
 
 pnpm i --ignore-workspace
 docker compose build --no-cache
